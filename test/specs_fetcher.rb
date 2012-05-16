81d35166ef805d9eea453b183fb889eb336a7bb1
Thomas Franquelin <you@example.com>
2012-05-16 18:31:21 -0400

    fix the specs_fetcher test

--- test/specs_fetcher.rb
+++ test/specs_fetcher.rb
@@ -4,7 +4,8 @@ require_relative '../lib/specs_fetcher'
 
 describe SpecsCrawler::SpecsFetcher do
   it 'returns an array of text contained in the tags matched by a css selector' do
-    @sample_file = File.read 'sample.html', encoding: 'utf-8'
+    @current_dir = File.dirname __FILE__
+    @sample_file = File.read(File.join(@current_dir, 'sample.html'), encoding: 'utf-8')
     @fetcher = SpecsCrawler::SpecsFetcher.new @sample_file
     @fetcher.fetch_content('#specs-table tr th span').must_equal ['Type de boîtier', 'Poids en kg', "Taille de l'écran"]
     @fetcher.fetch_content('#specs-table tr td span').must_equal ['Portable - Notebook', '2,6 Kg', '15"'] 
